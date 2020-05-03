c0ca8aa36d0cfefc26581ba8ca2432e7c493613c
TrueVincentVega <drpeso.jr@gmail.com>
2020-05-03 10:26:17 +0300

    Open and save file in simple window notepad

---
+++ SimpleWindow/main.cpp
@@ -0,0 +1,280 @@
+#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR szFilter[] = "Text files (*.txt)\0*.txt\0All files (*.*)\0*.*\0"; // cconst filter
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL	CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL	LoadTextFileToEdit(HWND hwnd, LPCTSTR pszFileName);
+BOOL	SaveTextFileFromEdit(HWND hwnd, LPCSTR pszFileName);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+	//1) Регистрация класса окна:
+	CONST CHAR SZ_CLASS_NAME[] = "myWindowClass";
+	WNDCLASSEX wc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	//wc.hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+	//wc.hIconSm = (HICON)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1), IMAGE_ICON, 16, 16, 0);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpszMenuName = NULL;//MAKEINTRESOURCE(IDR_MENU1);
+	wc.lpszClassName = SZ_CLASS_NAME;
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	//2) Создание окна:
+	HWND hwnd = CreateWindowEx
+	(
+		WS_EX_CLIENTEDGE,
+		SZ_CLASS_NAME,
+		"Title of my Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 640, 480,
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window was not created", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	//3) Message loop (цикл сообщений):
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		//Creating menu:
+		HMENU hMenu = CreateMenu();
+		HMENU hSubMenu;
+
+		hSubMenu = CreatePopupMenu();
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_NEW, "&New");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_OPEN, "&Open");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_SAVE, "&Save");
+		AppendMenu(hSubMenu, MF_STRING, ID_FILE_EXIT, "E&xit");
+		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&File");
+
+		hSubMenu = CreatePopupMenu();
+		AppendMenu(hSubMenu, MF_STRING, ID_HELP_ABOUT, "&About");
+		AppendMenu(hMenu, MF_STRING | MF_POPUP, (UINT)hSubMenu, "&Help");
+
+		SetMenu(hwnd, hMenu);
+
+		//Adding icons:
+		HICON hIcon = (HICON)LoadImage(NULL, "Document.ico", IMAGE_ICON, 32, 32, LR_LOADFROMFILE);
+		SendMessage(hwnd, WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+
+			//Text editor:
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+
+			HWND hEdit = CreateWindowEx
+			(
+				WS_EX_CLIENTEDGE, "Edit", "",
+				WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
+				0, 0, rect.right, rect.bottom,
+				hwnd,
+				(HMENU)IDC_MAIN_EDIT,
+				GetModuleHandle(NULL),
+				NULL
+			);
+		if (hEdit == NULL)
+			MessageBox(hwnd, "Can not create edit control", "Error", MB_OK | MB_ICONERROR);
+
+		// Красивый шрифт для написания текста в окне
+		HFONT hDefault = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+		SendMessage(hEdit, WM_SETFONT, (WPARAM)hDefault, MAKELPARAM(FALSE, 0));
+	}
+	break;
+
+	case WM_SIZE: //для того чтобы при растягивании окна скролбары менялись с ним
+	{
+		RECT rcClient;
+		GetClientRect(hwnd, &rcClient);
+		HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+		SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_OPEN:
+		{
+			// Создадим стандартное окно открытия файла:
+			OPENFILENAME ofn;
+			CHAR szFileName[MAX_PATH]{};
+
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = szFilter; // filter
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH; // максим. длинна пути
+			ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+			//ofn.lpstrDefExt = "txt";
+			if (GetOpenFileName(&ofn))
+			{
+				// Происходит загрузка файла...
+				HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+				LoadTextFileToEdit(hEdit, szFileName);
+			}
+			
+		}
+		break;
+		case ID_FILE_SAVE:
+		{
+			OPENFILENAME ofn;
+			CHAR szFileName[MAX_PATH]{};
+
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = szFilter;
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+			ofn.lpstrDefExt = "txt";
+
+			if (GetSaveFileName(&ofn))
+			{
+				HWND hEdit = GetDlgItem(hwnd, IDC_MAIN_EDIT);
+				SaveTextFileFromEdit(hEdit, szFileName);
+			}
+		}
+		break;
+		case ID_FILE_EXIT:
+			DestroyWindow(hwnd);
+			break;
+		case ID_HELP_ABOUT:
+		{
+			switch (DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_ABOUT), hwnd, DlgProc))
+			{
+			case IDOK:		MessageBox(hwnd, "Dialog ended with OK!", "Info", MB_OK | MB_ICONINFORMATION);		break;
+			case IDCANCEL:	MessageBox(hwnd, "Dialog ended with Cancel!", "Info", MB_OK | MB_ICONINFORMATION);	break;
+			case -1:		MessageBox(hwnd, "Dialog Failed!", "Error", MB_OK | MB_ICONERROR);		break;
+			}
+		}
+		break;
+		}
+		break;
+	case WM_CLOSE:
+		//if (MessageBox(hwnd, "Вы действительно хотите закрыть окно", "Вы уверены?", MB_YESNO|MB_ICONQUESTION) == IDYES)
+	{
+		DestroyWindow(hwnd);
+	}
+	break;
+	case WM_DESTROY:
+		//MessageBox(hwnd, "От странные, лчше б дверь закрыли", "Возмущение", MB_OK | MB_ICONINFORMATION);
+		PostQuitMessage(0);
+		break;
+	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+BOOL	CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			EndDialog(hwnd, IDOK);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, IDCANCEL);
+			break;
+		}
+		break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
+	default:return FALSE;
+	}
+	return TRUE;
+}
+
+BOOL	LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+	BOOL bSuccess = FALSE;
+
+	HANDLE hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	if (hFile != INVALID_HANDLE_VALUE) //Если файл создался продолжаем работу сним
+	{
+		DWORD dwFileSize = GetFileSize(hFile, NULL);
+		if (dwFileSize != UINT_MAX)
+		{
+			LPSTR pszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			if (pszFileText)
+			{
+				DWORD dwRead;
+
+				if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
+				{
+					pszFileText[dwFileSize] = 0;
+					if (SetWindowText(hEdit, pszFileText))bSuccess = TRUE;
+				}
+				GlobalFree(pszFileText);
+			}
+
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccess;
+}
+
+BOOL	SaveTextFileFromEdit(HWND hEdit, LPCSTR pszFileName)
+{
+	BOOL bSuccess = FALSE;
+	HANDLE hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwTextLength = GetWindowTextLength(hEdit);
+		if (dwTextLength > 0)
+		{
+			LPSTR pszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+			if (pszText != NULL)
+			{
+				if (GetWindowText(hEdit, pszText, dwTextLength + 1))
+				{
+					DWORD dwWritten;
+					if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))bSuccess = TRUE;
+				}
+				GlobalFree(pszText);
+			}
+			CloseHandle(hFile);
+		}
+	}
+	return bSuccess;
+}
---
+++ SimpleWindow/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется Resource.rc
+//
+#define IDR_MENU1                       101
+#define IDI_ICON1                       102
+#define IDD_DIALOG1                     103
+#define IDD_ABOUT                       103
+#define IDR_MENU2                       105
+#define IDC_MAIN_EDIT                   105
+#define ID_HELP_ABOUT                   40001
+#define ID_FILE_NEW                     40002
+#define ID_FILE_OPEN                    40003
+#define ID_FILE_SAVE                    40004
+#define ID_FILE_SAVEAS                  40005
+#define ID_FILE_EXIT                    40006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
